041ca99704f6a7a6a94f94b01eb9df5fce23cf87
Albinpirela <albinpirela40@gmail.com>
2023-05-24 09:02:21 -0400

    Add div container

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 // import _ from 'lodash';
+import _ from 'lodash';
 import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  const heading = document.createElement('h1');
-  heading.innerHTML = 'Hello webpack';
-  heading.classList.add('hello');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // Add the image to our existing div.
 
   return element;
