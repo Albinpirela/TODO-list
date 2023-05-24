148b5eb46e09eacf526d87aed78524b5f3e4913d
Albinpirela <albinpirela40@gmail.com>
2023-05-24 16:05:09 -0400

    Test3

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: './src/index.html',
+      filename: './index.html',
     }),
   ],
   output: {
