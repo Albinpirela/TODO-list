c426e4b65b9040ee088441bf3a0fe7b1be6092a5
Albinpirela <albinpirela40@gmail.com>
2023-05-24 14:14:38 -0400

    Test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-// import _ from 'lodash';
-import _ from 'lodash';
 import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // Add the image to our existing div.
-
+  const heading = document.createElement('h1');
+  heading.innerHTML = 'Hello webpack';
+  heading.classList.add('hello');
   return element;
 }
 
